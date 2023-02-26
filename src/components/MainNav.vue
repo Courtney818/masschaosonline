317f59a0dffd4841483a1c01145884c9c10e89ef
Courtney818 <courtneyerickson818@yahoo.com>
2023-02-26 00:13:32 -0600

    first components

---
+++ src/components/MainNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  data: () => ({}),
+  methods: {
+    //
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
