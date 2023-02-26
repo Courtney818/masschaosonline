7bbb833b05c9dd945fbbff1d84daea7db9491ba4
Courtney818 <courtneyerickson818@yahoo.com>
2023-02-25 22:26:58 -0600

    tailwind update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container>
+  <v-container class="color-black">
+    <p class="text-chaosorange">What color</p>
+    <p class="text-chaosgreen">What color</p>
+    <p class="text-chaosblue">What color</p>
   </v-container>
 </template>
 
